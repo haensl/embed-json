c2d5431377555b4b175ad2ac1cee68f0012a24a0
HP Dietz <hp@jandh.org>
2018-03-14 22:17:41 +0100

    WIP: unit tests.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # emed-json
 Node.js module to embed JSON data from files into html.
+
+## Installation
+
+### NPM
+  `npm i --save embed-json`
+
+### Yarn
+  `yarn add embed-json`
+
+## Usage
+
+### Synopsis
+
+```javascript
+/**
+ * Embed JSON data from referenced files into the given HTML string.
+ * When embedJson() encounters a script tag with JSON mime type and a src attribute,
+ * e.g.
+ *  <script type="application/json" src="data.json"></script>
+ * it will retrieve the data from the file and embed it into the tag, while removing
+ * the src attribute.
+ */
+embedJson(htmlString, options);
+```
+
+### Example
+
+```javascript
+const embedJson = require('embed-json');
+const fs = require('fs');
+
+const html = fs.readFileSync('index.html', 'utf8');
+embedJson(html);
+```
+
+### 
---
+++ test/specs.js
@@ -0,0 +1,103 @@
+const join = require('path').join;
+const fs = require('fs');
+const expect = require('chai').expect;
+const embedJson = require('../');
+const fixturesPath = join(__dirname, 'fixtures');
+
+const fixture = (name) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(join(fixturesPath, name), 'utf8', (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(data);
+    });
+  });
+
+describe('embed-json', () => {
+  describe('script with src attribute', () => {
+    let output;
+    
+    describe('and mime type application/json', () => {
+      beforeEach((done) => {
+        fixture('json.html')
+          .then((html) => {
+            output = embedJson(html);
+            done();
+          });
+      });
+
+      it('inserts the JSON data into the script tag', () => {
+        expect(/"foo":"bar"/.test(output)).to.be.true;
+      });
+
+      it('removes the src attribute', () => {
+        expect(/src=/.test(output)).to.be.false;
+      });
+    });
+
+    describe('and mime type application/ld+json', () => {
+      beforeEach((done) => {
+        fixture('ld+json.html')
+          .then((html) => {
+            output = embedJson(html);
+            done();
+          });
+      });
+
+      it('inserts the JSON data into the script tag', () => {
+        expect(/"foo":"bar"/.test(output)).to.be.true;
+      });
+
+      it('removes the src attribute', () => {
+        expect(/src=/.test(output)).to.be.false;
+      });
+    });
+  });
+
+  describe('no script tag', () => {
+    let output;
+    let input;
+
+    beforeEach((done) => {
+      fixture('no-script.html')
+        .then((html) => {
+          input = html;
+          output = embedJson(html);
+          done();
+        });
+    });
+
+    it('bypasses the input', () => {
+      expect(output).to.equal(input);
+    });
+  });
+
+  describe('no input', () => {
+    it('throws', () => {
+      expect(embedJson).to.throw;
+    });
+  });
+
+  describe('non-existent src', () => {
+    let error;
+
+    beforeEach((done) => {
+      fixture('nonexistent-src.html')
+        .then((html) => {
+          try {
+            embedJson(html);
+          } catch (err) {
+            error = err;
+          } finally {
+            done();
+          }
+        });
+    });
+
+    it('throws an invalid source error', () => {
+      expect(/invalid source/i.test(error.message)).to.be.true;
+    });
+  });
+});
